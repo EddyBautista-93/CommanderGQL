ab941c8836d3c633a71a9ffc3af80577160dd492
EddyBautista <bautista.j.eddy@gmail.com>
2021-09-27 06:17:47 -0500

    setting up graphql and testing db

---
+++ GraphQL/Query.cs
@@ -0,0 +1,16 @@
+using System.Linq;
+using CommanderGQL.Data;
+using CommanderGQL.Models;
+using HotChocolate;
+using System.Linq;
+
+namespace CommanderGQL.GraphQL
+{
+    public class Query
+    {
+        public IQueryable<Platform> GetPlatform([Service] AppDbContext context)
+        {
+            return context.Platforms;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,21 +8,26 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
+using CommanderGQL.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommanderGQL
 {
     public class Startup
     {
         private readonly IConfiguration Configuration;
+        private string conn = null;
 
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer
+            (Configuration.GetConnectionString("CommandConStr")));
+            conn = Configuration["conn"];
             
         }
 
@@ -43,6 +48,11 @@ namespace CommanderGQL
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+            app.Run(async (context) =>
+            {
+            var result = string.IsNullOrEmpty(conn) ? "Null" : "Not Null";
+            await context.Response.WriteAsync($"Secret is {result}");
+        });
         }
     }
 }
